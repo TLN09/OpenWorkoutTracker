8d3c1cf9882d72ba4f6077ad2f0e9ad3ea11ab91
TLN09 <59337970+TLN09@users.noreply.github.com>
2024-10-15 19:18:23 +0200

    Initial Commit

---
+++ OpenWorkoutTracker/OpenWorkoutTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  OpenWorkoutTrackerApp.swift
+//  OpenWorkoutTracker
+//
+//  Created by Thomas Lind Nielsen on 15/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OpenWorkoutTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ OpenWorkoutTrackerTests/OpenWorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWorkoutTrackerTests.swift
+//  OpenWorkoutTrackerTests
+//
+//  Created by Thomas Lind Nielsen on 15/10/2024.
+//
+
+import Testing
+@testable import OpenWorkoutTracker
+
+struct OpenWorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
